f449d850524b0230b363ba3558c406211150dd89
kuro <toshras22@gmail.com>
2021-10-07 20:46:32 +0900

    GameDecisionモデル作成

--- blackJack.js
+++ blackJack.js
@@ -96,6 +96,21 @@ class Tool {
   }
 }
 
+class GameDecision {
+  /*
+       String action : プレイヤーのアクションの選択。（ブラックジャックでは、hit、standなど。）
+       Number amount : プレイヤーが選択する数値。
+
+       これはPlayer.promptPlayer()は常にreturnする、標準化されたフォーマットです。
+    */
+  constructor(action, amount) {
+    // アクション
+    this.action = action;
+    // プレイヤーが選択する数値
+    this.amount = amount;
+  }
+}
+
 let deck = new Deck();
 deck.resetDeck();
 deck.shuffle();
